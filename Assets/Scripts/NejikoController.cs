12d299c67427e7bc7ecf7bbc4d08e94a197e3b2c
origamimus <274768565+guiyuku79-gif@users.noreply.github.com>
2026-07-26 14:14:10 +0900

    キャラクターを動かす

--- Assets/Scripts/NejikoController.cs
+++ Assets/Scripts/NejikoController.cs
@@ -1,16 +1,75 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class NejikoController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    const int MinLane = -2;
+    const int MaxLane = 2;
+    const float Lanewidth = 1.0f;
+    public float Gravity;
+    public float SpeedZ;
+    public float SpeedX;
+    public float SpeedJump;
+    public float AccelerationZ;
+
+    CharacterController _controller;
+    Animator _animator;
+    Vector3 _moveDirection = Vector3.zero;
+    int _targetLane;
+    InputAction _jumpAction;
+    InputAction _moveAction;
+
     void Start()
     {
-        
+        _jumpAction = InputSystem.actions.FindAction("Jump");
+        _moveAction = InputSystem.actions.FindAction("Move");
+        _jumpAction.Enable();
+        _moveAction.Enable();
+
+        _controller = GetComponent<CharacterController>();
+        _animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(Keyboard.current.leftArrowKey.wasPressedThisFrame) MoveToLeft();
+        if(Keyboard.current.rightArrowKey.wasPressedThisFrame) MoveToRight();
+        if(Keyboard.current.spaceKey.wasPressedThisFrame) Jump();
+
+        float acceleratedZ = _moveDirection.z +(AccelerationZ * Time.deltaTime);
+        _moveDirection.z = Mathf.Clamp(acceleratedZ, 0, SpeedZ);
+
+        float ratioX = (_targetLane * Lanewidth - transform.position.x) / Lanewidth;
+        _moveDirection.x = ratioX * SpeedX;
+
+        _moveDirection.y -= Gravity * Time.deltaTime;
+
+        Vector3 globalDirection = transform.TransformDirection(_moveDirection);
+        _controller.Move(globalDirection*Time.deltaTime);
+
+        if(_controller.isGrounded) _moveDirection.y = 0;
+
+        _animator.SetBool("run",_moveDirection.z > 0.0f);
+    }
+
+    public void MoveToLeft()
+    {
+        if(_controller.isGrounded && _targetLane > MinLane) _targetLane--;
+    }
+
+    public void MoveToRight()
+    {
+        if(_controller.isGrounded && _targetLane < MaxLane) _targetLane++;
+    }
+
+    public void Jump()
+    {
+        if (_controller.isGrounded)
+        {
+            _moveDirection.y = SpeedJump;
+
+            _animator.SetTrigger("jump");
+        }
     }
 }
---
+++ Assets/Scripts/SimpleFollow.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class SimpleFollow : MonoBehaviour
+{
+    public GameObject Target;
+    public float FollowSpeed;
+
+    Vector3 _diff;
+
+    void Start()
+    {
+        _diff = Target.transform.position - transform.position;
+    }
+    void LateUpdate()
+    {
+        transform.position = Vector3.Lerp(
+            transform.position,
+            Target.transform.position-_diff,
+            Time.deltaTime * FollowSpeed
+        );
+    }
+}
